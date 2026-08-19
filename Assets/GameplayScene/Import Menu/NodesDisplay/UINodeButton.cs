6a0f233e7ab50db90ab655f40492b7aa72131e73
Ben Mills <benjamin.scott.mills@gmail.com>
2026-08-19 03:59:20 -0400

    Fix issue with node buttons not being properly circular

--- Assets/GameplayScene/Import Menu/NodesDisplay/UINodeButton.cs
+++ Assets/GameplayScene/Import Menu/NodesDisplay/UINodeButton.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UINodeButton : GenericButton
+public class UINodeButton : GenericButton, ICanvasRaycastFilter
 {
     public Image highlight;
     public Reducer reducer;
     public TooltipText tooltipText;
     public UIReducerVisual reducerVisual;
+    public RectTransform rectTransform;
     public bool enableHighlight;
     public bool useRawName;
+
+    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+        Vector3[] cornersArray = new Vector3[4];
+        rectTransform.GetWorldCorners(cornersArray);
+
+        return (cornersArray[2].x - cornersArray[1].x) / 2 >= Vector2.Distance(eventCamera.ScreenToWorldPoint(sp), transform.position);
+    }
 
     void Update()
     {
